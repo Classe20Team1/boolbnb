71afc70b9a6e7e038700016dd486692d968ea4e3
Matteo Ponchietti <matteoponchietti@gmail.com>
2021-02-26 21:48:15 +0000

    Auto stash before checking out "HEAD"

---
+++ resources/views/apartments/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        @foreach($apartments as $apartment)
+    
+        <li>
+            <h1>
+                {{ $apartment->title }}
+            </h1>
+            <h4>
+                {{ $apartment->user->username }}
+            </h4>
+            <h6>
+                {{ $apartment->user->info->address }}
+            </h6>
+            <p>
+                {{ $apartment->description }}
+            </p>
+
+        </li>
+    
+        @endforeach
+
+    </ul>
+</body>
+</html>
