1c8f7723f859ce0cdfa369be52206f08633228cd
Francesco Maestroni <francescomaestroni1@gmail.com>
2021-03-01 15:59:57 +0100

    Aggiunto header layout

--- public/js/app.js
+++ public/js/app.js
@@ -49995,8 +49995,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Boolean\BackEnd\boolbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Boolean\BackEnd\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/francesco/Documents/Coding/esercizi-boolean/boolbnb/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/francesco/Documents/Coding/esercizi-boolean/boolbnb/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/views/layouts/boolbnb.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BoolBnb - @yield('title')</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+  </head>
+  <body>
+
+    <header>
+      <nav>
+        <div class="nav-logo-box">
+          <img src="{{ asset('img/logo.png') }}" alt="logo">
+        </div>
+        <div class="nav-contents">
+          <ul>
+            <li>
+              Appartamenti
+              <div class="underline"></div>
+            </li>
+            <li>
+              Ricerca avanzata
+              <div class="underline"></div>
+            </li>
+          </ul>
+        </div>
+        <div class="nav-user">
+          <ul class="nav-right-list-1">
+            <li>Diventa un host</li>
+            <li>
+              <i class="fas fa-globe"></i>
+            </li>
+          </ul>
+          <ul class="nav-right-list-2">
+            <li>
+              <i class="fas fa-bars bars-icon"></i>
+              <i class="fas fa-user-circle user-icon"></i>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div class="hero">
+        <div class="hero-img-box">
+          <img src="{{ asset('img/hero.jpg') }}" alt="logo">
+        </div>
+        <h1>Grazie agli host puoi.</h1>
+        <div class="search-bar">
+          <form class="search-bar-form" action="index.html" method="post">
+            <div class="left-input">
+              <label for="">Dove</label><br>
+              <input type="text" name="city" value="" placeholder="Dove vuoi andare?" required>
+            </div>
+            <div class="right-input">
+              <div class="">
+                <label for="">Ospiti</label><br>
+                <input type="text" name="guests" value="" placeholder="Aggiungi ospiti" required>
+              </div>
+              <div class="">
+                <button type="submit" name="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </header>
+
+  </body>
+</html>
