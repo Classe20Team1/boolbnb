c8322b91c49016480aa90757233666f84ed4874f
Francesco Maestroni <francescomaestroni1@gmail.com>
2021-03-03 23:29:15 +0100

    Aggiunta view My-Apartments

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('my-apartments');
 });
 Route::get('/search', function () {
     return view('search');
